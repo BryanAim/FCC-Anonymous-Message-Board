c44265224773b88fc7793fa883f5793c981f5fb0
Isale <isale.bryan@gmail.com>
2020-06-29 10:13:47 +0300

    basic CRUD operations

--- routes/api.js
+++ routes/api.js
@@ -7,13 +7,26 @@
 */
 
 'use strict';
+const ThreadHandler = require('../controllers/threadHandler');
+const RepliesHandler = require('../controllers/repliesHandler')
 
 var expect = require('chai').expect;
 
 module.exports = function (app) {
+
+  const threadHandler = new ThreadHandler();
+  const repliesHandler = new RepliesHandler();
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+  .post(threadHandler.newThread)
+  .get(threadHandler.threadList)
+  .put(threadHandler.reportThread)
+  .delete(threadHandler.deleteThread);
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+  .post(repliesHandler.addReply)
+  .get(repliesHandler.replyList)
+  .put(repliesHandler.reportReply)
+  .delete(repliesHandler.deleteReply);
 
 };
